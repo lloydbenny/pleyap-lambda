38e5c2513d6d59e456f0952ebe2f140c000e1f7f
lloydbenny <bennihynns@gmail.com>
2020-11-19 20:13:35 +0100

    changes troy

--- express/server.js
+++ express/server.js
@@ -6,40 +6,136 @@ const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
 
-const ApolloClient = require('apollo-client').ApolloClient;
-const gql = require('graphql-tag');
-const fetch = require('cross-fetch/polyfill').fetch;
-const createHttpLink = require('apollo-link-http').createHttpLink;
-const InMemoryCache = require('apollo-cache-inmemory').InMemoryCache;
+// const ApolloClient = require('apollo-client').ApolloClient;
+// const gql = require('graphql-tag');
+// const fetch = require('cross-fetch/polyfill').fetch;
+// const createHttpLink = require('apollo-link-http').createHttpLink;
+// const InMemoryCache = require('apollo-cache-inmemory').InMemoryCache;
 
 const router = express.Router();
 
-const client = new ApolloClient({
-  link: createHttpLink({
-    uri: 'https://pleyap.hasura.app/v1/graphql',
-    fetch: fetch,
-    headers: {
-      "content-type": "application/json",
-      "x-hasura-admin-secret": "Ssiigo@01"
+// const client = new ApolloClient({
+//   link: createHttpLink({
+//     uri: 'https://pleyap.hasura.app/v1/graphql',
+//     fetch: fetch,
+//     headers: {
+//       "content-type": "application/json",
+//       "x-hasura-admin-secret": "Ssiigo@01"
+//     },
+//   }),
+//   cache: new InMemoryCache()
+// });
+
+router.post('/hasura-user-sync-registration-pleyap', (req, res) => {
+
+  const userId = req.body.event.user.id;
+  const firstName = req.body.event.user.firstName;
+  const lastName = req.body.event.user.lastName;
+  const picture = req.body.event.user.imageUrl;
+
+  console.log(userId + "\n" + firstName + "\n" + lastName + "\n" + picture);
+
+  const mutation = `mutation (
+    $user_id: String,
+		$last_name: String,
+		$first_name: String,
+		$profileUrl: String) {
+      insert_pleyap_datastore_Profile(objects: [{
+        user_id: $user_id,
+        lastName: $last_name,
+        firstName: $first_name
+        profileUrl: $profileUrl
+      }],
+        on_conflict: {
+          constraint: profile_pk,
+          update_columns: [last_seen, lastName, firstName]
+        }) {
+        affected_rows
+      }
+  }`;
+
+  request.post(
+    {
+      headers: {
+        "content-type": "application/json",
+        "x-hasura-admin-secret": "Ssiigo@01"
+      },
+      url: "https://pleyap.hasura.app/v1/graphql",
+      body: JSON.stringify({
+        query: mutation,
+        variables: {
+          "user_id": userId,
+          "first_name": firstName,
+          "last_name": lastName,
+          "profileUrl": picture
+        }
+      })
     },
-  }),
-  cache: new InMemoryCache()
+    function(error, response, body) {
+      console.log(body);
+      console.error(error);
+    }
+  );
+
+  res
+	.status(200)
+	.json({ postBody: req.body.event });
 });
 
-router.post('/hasura-user-sync-registration', (req, res) => {
+router.post('/hasura-user-sync-registration-pleyap-admin', (req, res) => {
 
-  const userRole = req.body.event.registration.roles[0];
+  const userId = req.body.event.user.id;
+  const firstName = req.body.event.user.firstName;
+  const lastName = req.body.event.user.lastName;
+  const picture = req.body.event.user.imageUrl;
 
-  console.log(userRole);
+  console.log(userId + "\n" + firstName + "\n" + lastName + "\n" + picture);
 
-  switch(userRole) {
-    case "barber_shop": {
-      addUserWithBarberShop(req);
+  const mutation = `mutation (
+    $user_id: String,
+		$last_name: String,
+		$first_name: String,
+		$profileUrl: String) {
+      insert_pleyap_datastore_Profile(objects: [{
+        user_id: $user_id,
+        lastName: $last_name,
+        firstName: $first_name
+        profileUrl: $profileUrl
+      }],
+        on_conflict: {
+          constraint: profile_pk,
+          update_columns: [last_seen, lastName, firstName]
+        }) {
+        affected_rows
+      }
+  }`;
+
+  request.post(
+    {
+      headers: {
+        "content-type": "application/json",
+        "x-hasura-admin-secret": "Ssiigo@01"
+      },
+      url: "https://pleyap.hasura.app/v1/graphql",
+      body: JSON.stringify({
+        query: mutation,
+        variables: {
+          "user_id": userId,
+          "first_name": firstName,
+          "last_name": lastName,
+          "profileUrl": picture
+        }
+      })
+    },
+    function(error, response, body) {
+      if (response.statusCode == 200) {
+        addBarberShop(req);
+      } else {
+        console.error(error);
+      }
+      console.log(response.statusCode);
     }
-    case "user": {
-      addUser(req);
-    }
-  }
+  );
 
   res
 	.status(200)
@@ -144,112 +240,6 @@ function addBarberShop(req) {
     function(error, response, body) {
       console.log(body);
       console.error(error);
-    }
-  );
-}
-
-function addUser(req) {
-  const userId = req.body.event.user.id;
-  const firstName = req.body.event.user.firstName;
-  const lastName = req.body.event.user.lastName;
-  const picture = req.body.event.user.imageUrl;
-
-  console.log(userId + "\n" + firstName + "\n" + lastName + "\n" + picture);
-
-  const mutation = `mutation (
-    $user_id: String,
-		$last_name: String,
-		$first_name: String,
-		$profileUrl: String) {
-      insert_pleyap_datastore_Profile(objects: [{
-        user_id: $user_id,
-        lastName: $last_name,
-        firstName: $first_name
-        profileUrl: $profileUrl
-      }],
-        on_conflict: {
-          constraint: profile_pk,
-          update_columns: [last_seen, lastName, firstName]
-        }) {
-        affected_rows
-      }
-  }`;
-
-  request.post(
-    {
-      headers: {
-        "content-type": "application/json",
-        "x-hasura-admin-secret": "Ssiigo@01"
-      },
-      url: "https://pleyap.hasura.app/v1/graphql",
-      body: JSON.stringify({
-        query: mutation,
-        variables: {
-          "user_id": userId,
-          "first_name": firstName,
-          "last_name": lastName,
-          "profileUrl": picture
-        }
-      })
-    },
-    function(error, response, body) {
-      console.log(body);
-      console.error(error);
-    }
-  );
-}
-
-function addUserWithBarberShop(req) {
-  const userId = req.body.event.user.id;
-  const firstName = req.body.event.user.firstName;
-  const lastName = req.body.event.user.lastName;
-  const picture = req.body.event.user.imageUrl;
-
-  console.log(userId + "\n" + firstName + "\n" + lastName + "\n" + picture);
-
-  const mutation = `mutation (
-    $user_id: String,
-		$last_name: String,
-		$first_name: String,
-		$profileUrl: String) {
-      insert_pleyap_datastore_Profile(objects: [{
-        user_id: $user_id,
-        lastName: $last_name,
-        firstName: $first_name
-        profileUrl: $profileUrl
-      }],
-        on_conflict: {
-          constraint: profile_pk,
-          update_columns: [last_seen, lastName, firstName]
-        }) {
-        affected_rows
-      }
-  }`;
-
-  request.post(
-    {
-      headers: {
-        "content-type": "application/json",
-        "x-hasura-admin-secret": "Ssiigo@01"
-      },
-      url: "https://pleyap.hasura.app/v1/graphql",
-      body: JSON.stringify({
-        query: mutation,
-        variables: {
-          "user_id": userId,
-          "first_name": firstName,
-          "last_name": lastName,
-          "profileUrl": picture
-        }
-      })
-    },
-    function(error, response, body) {
-      if (response.statusCode == 200) {
-        addBarberShop(req);
-      } else {
-        console.error(error);
-      }
-      console.log(response.statusCode);
     }
   );
 }
