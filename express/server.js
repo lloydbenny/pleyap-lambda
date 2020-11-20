fd3de1209590592e5c67b6bee87cf235822b8adc
lloydbenny <bennihynns@gmail.com>
2020-11-20 07:27:02 +0100

    back

--- express/server.js
+++ express/server.js
@@ -128,11 +128,11 @@ router.post('/hasura-user-sync-registration-pleyap-admin', (req, res) => {
       })
     },
     function(error, response, body) {
-      // if (response.statusCode == 200) {
-      //   addBarberShop(req);
-      // } else {
-      //   console.error(error);
-      // }
+      if (response.statusCode == 200) {
+        addBarberShop(req);
+      } else {
+        console.error(error);
+      }
       console.log(response.statusCode);
     }
   );
